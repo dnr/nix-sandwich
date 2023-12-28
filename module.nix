70009f1208376b3cef81cdf8d02d732dcd119fd8
David Reiss <dnr@dnr.im>
2023-12-28 00:17:30 +0000

    improve module

--- module.nix
+++ module.nix
@@ -1,33 +1,55 @@
 { config, lib, pkgs, ... }:
 let
-  pkg = (import ./. { inherit pkgs; }).nix-sandwich-local;
-in {
-  # TODO: make a proper module with options instead of always-on
-
-  nix.settings = {
-    trusted-substituters = [ "http://localhost:7419" ];
-    # TODO: These are needed since we don't cache "recents" across restarts.
-    # After we can do that, remove this.
-    narinfo-cache-positive-ttl = 15;
-    narinfo-cache-negative-ttl = 15;
+  defpkg = (import ./. { inherit pkgs; }).nix-sandwich-local;
+  cfg = config.programs.nix-sandwich;
+in with lib; {
+  options = {
+    programs.nix-sandwich = {
+      enable = mkEnableOption "nix download helper";
+      differ = mkOption {
+        description = "url to remote differ";
+        type = types.str;
+        default = throw "must set config.programs.nix-sandwich.differ!";
+        example = "https://abc213";
+      };
+      port = mkOption {
+        description = "port to listen on";
+        type = types.int;
+        default = 7419;
+      };
+      package = mkOption {
+        description = "nix-sandwich package";
+        type = types.package;
+        default = defpkg;
+      };
+    };
   };
 
-  systemd.sockets.nix-sandwich = {
-    description = "nix download helper activation socket";
-    wantedBy = [ "sockets.target" ];
-    socketConfig.ListenStream = "127.0.0.1:7419";
-  };
+  config = mkIf cfg.enable {
+    nix.settings = {
+      trusted-substituters = [ "http://localhost:${toString cfg.port}" ];
+      # TODO: These are needed since we don't cache "recents" across restarts.
+      # After we can do that, remove this.
+      narinfo-cache-positive-ttl = 15;
+      narinfo-cache-negative-ttl = 15;
+    };
 
-  systemd.services.nix-sandwich = {
-    description = "nix download helper";
-    serviceConfig.ExecStart = "${pkg}/bin/nix-sandwich";
-    serviceConfig.Type = "notify";
-    serviceConfig.DynamicUser = true;
-    serviceConfig.LogsDirectory = "nix-sandwich-analytics";
-    serviceConfig.TemporaryFileSystem = "/tmpfs:size=16G,mode=1777"; # force tmpfs
-    environment.nix_sandwich_subst_idle_time = "15m";
-    environment.nix_sandwich_differ = lib.mkDefault (
-      throw "must override systemd.services.nix-sandwich.environment.nix_sandwich_differ!");
-    environment.TMPDIR = "/tmpfs";
+    systemd.sockets.nix-sandwich = {
+      description = "nix download helper activation socket";
+      wantedBy = [ "sockets.target" ];
+      socketConfig.ListenStream = "127.0.0.1:${toString cfg.port}";
+    };
+
+    systemd.services.nix-sandwich = {
+      description = "nix download helper";
+      serviceConfig.ExecStart = "${cfg.package}/bin/nix-sandwich";
+      serviceConfig.Type = "notify";
+      serviceConfig.DynamicUser = true;
+      serviceConfig.LogsDirectory = "nix-sandwich-analytics";
+      serviceConfig.TemporaryFileSystem = "/tmpfs:size=16G,mode=1777"; # force tmpfs
+      environment.nix_sandwich_subst_idle_time = "15m";
+      environment.nix_sandwich_differ = cfg.differ;
+      environment.TMPDIR = "/tmpfs";
+    };
   };
 }
